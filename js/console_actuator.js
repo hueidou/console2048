9be25c94fd6251036ae3dc66bf8b40cf26871c40
hueidou <hueidou163@gmail.com>
2014-04-02 00:50:06 +0800

    hjkl~

--- js/console_actuator.js
+++ js/console_actuator.js
@@ -1,28 +1,49 @@
 function HTMLActuator() {
-  this.tileContainer    = document.querySelector(".tile-container");
-  this.scoreContainer   = document.querySelector(".score-container");
-  this.bestContainer    = document.querySelector(".best-container");
-  this.messageContainer = document.querySelector(".game-message");
+  //this.tileContainer    = document.querySelector(".tile-container");
+  //this.scoreContainer   = document.querySelector(".score-container");
+  //this.bestContainer    = document.querySelector(".best-container");
+  //this.messageContainer = document.querySelector(".game-message");
+  this.styledic = {
+    2: {'str': ' 2  ', 'c': 'black', 'bc': '#eee4da'},
+    4: {'str': ' 4  ', 'c': 'black', 'bc': '#ede0c8'},
+    8: {'str': ' 8  ', 'c': '#f9f6f2', 'bc': '#f2b179'},
+    16: {'str': ' 16 ', 'c': '#f9f6f2', 'bc': '#f59563'},
+    32: {'str': ' 32 ', 'c': '#f9f6f2', 'bc': '#f67c5f'},
+    64: {'str': ' 64 ', 'c': '#f9f6f2', 'bc': '#f65e3b'},
+    128: {'str': '128 ', 'c': '#f9f6f2', 'bc': '#edcf72'},
+    256: {'str': '256 ', 'c': '#f9f6f2', 'bc': '#edcc61'},
+    512: {'str': '512 ', 'c': '#f9f6f2', 'bc': '#edc850'},
+    1024: {'str': '1024', 'c': '#f9f6f2', 'bc': '#edc53f'},
+    2048: {'str': '2048', 'c': '#f9f6f2', 'bc': '#edc22e'},
+    'game-on': {'str': 'You win!', 'c': 'red', 'bc': 'white'},
+    'game-over': {'str': 'Game over!', 'c': 'red', 'bc': 'white'},
+  };
 
   this.score = 0;
+  this.values = [];
 }
 
 HTMLActuator.prototype.actuate = function (grid, metadata) {
   var self = this;
 
   window.requestAnimationFrame(function () {
-    self.clearContainer(self.tileContainer);
+    //!self.clearContainer(self.tileContainer);
+    self.clearContainer(null);
 
     grid.cells.forEach(function (column) {
       column.forEach(function (cell) {
-        if (cell) {
-          self.addTile(cell);
-        }
+        //if (cell) {
+        //  self.addTile(cell);
+        //}
+        self.write(cell);
       });
+      self.flush();
     });
 
-    self.updateScore(metadata.score);
-    self.updateBestScore(metadata.bestScore);
+    //self.updateScore(metadata.score);
+    //self.updateBestScore(metadata.bestScore);
+    self.writeConsole(['Score:', metadata.score], ['""', '""']);
+    self.writeConsole(['BestScore:', metadata.bestScore], ['""', '""']);
 
     if (metadata.terminated) {
       if (metadata.over) {
@@ -37,15 +58,17 @@ HTMLActuator.prototype.actuate = function (grid, metadata) {
 
 // Continues the game (both restart and keep playing)
 HTMLActuator.prototype.continueGame = function () {
-  this.clearMessage();
+  this.clearConsole();
 };
 
 HTMLActuator.prototype.clearContainer = function (container) {
-  while (container.firstChild) {
-    container.removeChild(container.firstChild);
-  }
+  //while (container.firstChild) {
+  //  container.removeChild(container.firstChild);
+  //}
+  this.clearConsole();
 };
 
+/*
 HTMLActuator.prototype.addTile = function (tile) {
   var self = this;
 
@@ -103,6 +126,7 @@ HTMLActuator.prototype.positionClass = function (position) {
   return "tile-position-" + position.x + "-" + position.y;
 };
 
+
 HTMLActuator.prototype.updateScore = function (score) {
   this.clearContainer(this.scoreContainer);
 
@@ -123,17 +147,51 @@ HTMLActuator.prototype.updateScore = function (score) {
 HTMLActuator.prototype.updateBestScore = function (bestScore) {
   this.bestContainer.textContent = bestScore;
 };
+*/
 
 HTMLActuator.prototype.message = function (won) {
   var type    = won ? "game-won" : "game-over";
   var message = won ? "You win!" : "Game over!";
 
-  this.messageContainer.classList.add(type);
-  this.messageContainer.getElementsByTagName("p")[0].textContent = message;
+  //this.messageContainer.classList.add(type);
+  //this.messageContainer.getElementsByTagName("p")[0].textContent = message;
+  this.writeConsole([message], ['"color: ' + this.styledic[type].c + ';background-color: ' + this.styledic[type].bc + ';"']);
 };
 
 HTMLActuator.prototype.clearMessage = function () {
   // IE only takes one value to remove at a time.
-  this.messageContainer.classList.remove("game-won");
-  this.messageContainer.classList.remove("game-over");
+  //this.messageContainer.classList.remove("game-won");
+  //this.messageContainer.classList.remove("game-over");
+};
+
+HTMLActuator.prototype.clearConsole = function () {
+  // console.clear();
+  console.clear();
+};
+
+HTMLActuator.prototype.write = function (cell) {
+  this.values.push(!!cell ? cell.value : null);
+};
+
+HTMLActuator.prototype.flush = function () {
+  var str = [];
+  var css = [];
+  for(var i in this.values) {
+    var num = this.values[i];
+    if (num != null) {
+      str.push('%c' + this.styledic[num].str);
+      css.push('"color: ' + this.styledic[num].c + ';background-color: ' + this.styledic[num].bc + ';border:#eee 1px solid"');
+    } else {
+      str.push('%c    ');
+      css.push('"background-color: #ddd;border:#eee 1px solid"');
+    }
+  }
+
+  this.writeConsole(str, css);
+  this.values = [];
+};
+
+HTMLActuator.prototype.writeConsole = function (str, css) {
+  var eval1 = 'console.log("' + str.join('') + '",' + css.join(',') + ')';
+  eval(eval1);
 };
